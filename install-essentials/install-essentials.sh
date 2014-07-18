9ca9f917e99935d3e940317d8b76fabef0dd2add
iakov <iakov@users.noreply.github.com>
2014-07-18 19:07:54 +0400

    Fixed install line

--- install-essentials/install-essentials.sh
+++ install-essentials/install-essentials.sh
@@ -6,7 +6,7 @@
 # 
 # Install wget (sudo apt-get install -y wget)
 # and run
-# wget -O - http://bit.ly/trikubuntu1407 | bash
+# cd ~ && wget http://bit.ly/trikubuntu1407 && source trikubuntu1407
 
 #Path for TRIK related sources
 TRIKSRC=~/trik-src
